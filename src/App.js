a67cbb12bf54e74ddf341e3e3e58d7ce1e105d31
Bruno <bruno.predot@gmail.com>
2020-04-01 15:13:36 +0200

    add new files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PureComponent from './Components/PureComponent';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+      <PureComponent />
       </header>
     </div>
   );
---
+++ src/Components/PureComponent.jsx
@@ -0,0 +1,17 @@
+/**
+ * @author Bruno
+ * Licence none
+ * First pure React component
+ */
+import React from "react";
+import './PureComponent.css';
+
+function PureComponent() {
+    return (
+        <div>
+            <h6 className={'purecomponent-h6'}>Pure Compo</h6>
+        </div>
+    )
+}
+
+export default PureComponent
