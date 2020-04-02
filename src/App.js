b67cf8cedb19782fe06d5983ab3fb23d4ffce851
Bruno <bruno.predot@gmail.com>
2020-04-02 17:37:47 +0200

    révision des bases

--- src/App.js
+++ src/App.js
@@ -5,28 +5,53 @@ import {PureComponent} from './Components/PureComponent';
 import {FunctionalPropsComponent} from './Components/PureComponent';
 import ClassComponent from "./Components/ClassComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <PureComponent />
-      <FunctionalPropsComponent test1={'la première props'}/>
-      <ClassComponent test2={'voici la props test2'}/>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        testAppState: ''
+    };
+    //arow function to bind this!
+    onInputChange = (val) => {
+        this.setState({testAppState: val});
+    };
+
+  render() {
+      let myLabels = [];
+      myLabels.push('bruno');
+      myLabels.push('énora');
+      myLabels.push('nila');
+      myLabels.push('papy');
+      myLabels.push('mamie');
+
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <div>
+                      <p>Voici les utilisateurs de cette session:</p>
+                      <ul>
+                          {myLabels.map( (label) =>  <li>{label}</li>  ) }
+                      </ul>
+                  </div>
+                  <img src={logo} className="App-logo" alt="logo" />
+                  {this.state.testAppState? <div></div> :
+                      <p>
+                          Edit <code>src/App.js</code> and save to reload.
+                      </p>}
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      Learn React
+                  </a>
+                  <PureComponent />
+                  <FunctionalPropsComponent test1={'la première props'}/>
+                  <ClassComponent test2={'voici la props test2'} onInputChange={this.onInputChange} nomApp={this.state.testAppState}/>
+              </header>
+          </div>
+      );
+  }
 }
 
 export default App;
--- src/Components/ClassComponent.jsx
+++ src/Components/ClassComponent.jsx
@@ -15,17 +15,21 @@ class ClassComponent extends React.Component{
         //this.handleChange = this.handleChange.bind(this);
     }
 
+    //arow function to bind this!
     handleChange = (e) => {
         //const value1 = e.target.value; // ou :
         const value2 = e.currentTarget.value;
         console.log(value2);
         this.setState({nom: value2});
+        this.props.onInputChange(e.currentTarget.value);// pour remonter notre val au composant app si on veut s'en servir ailleurs
     };
 
     render() {
         return(
             <div>
-                <h6 className={'classcomponent-h6'}>Je suis un composant avec son state, je passe une props : {this.props.test2}</h6>
+                <h6 className={'classcomponent-h6'}>Je suis un composant avec son state, je passe une props : {this.props.test2}<br/>
+                J'ai  également une props lié mon state composant : {this.state.nom}<br/>
+                Et la même props-state remontée de mon composant vers App : {this.props.nomApp}</h6>
                 <input type={'text'} value={this.state.nom} onChange={this.handleChange}/>
             </div>
         )
